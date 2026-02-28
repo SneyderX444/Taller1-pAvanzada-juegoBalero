986f08fd673c4ca2a285e5201c37d9c0622f68dc
User <User@XD>
2026-02-28 02:41:17 -0500

    Archivo de propiedades con los datos de los grupos y jugadores

---
+++ src/Modelo/Conexion_Propiedades.java
@@ -0,0 +1,14 @@
+package Modelo;
+
+import java.util.Properties;
+
+public class Conexion_Propiedades {
+    Properties archivoProp = new Properties();
+
+    public Conexion_Propiedades() {
+    }
+    
+    
+    
+    
+}
