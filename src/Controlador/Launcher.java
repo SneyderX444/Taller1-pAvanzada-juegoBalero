a9d0c978d7b2937bad786cf4d535593c483dd35b
User <User@XD>
2026-02-28 12:23:17 -0500

    Método main en el controlador.

---
+++ src/Controlador/Launcher.java
@@ -0,0 +1,13 @@
+package Controlador;
+
+public class Launcher {
+    
+/**
+ * Clase con el método main que tiene la única función de disparar el programa.
+ * @param args argumentos de línea de comandos
+*/
+    public static void main(String[] args){
+    new ControlPrincipal();
+    }
+    
+}
